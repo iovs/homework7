2f62ee1629101af932afd7f8431426a4c4665788
iovs <ovsyannikovio@gmail.com>
2025-06-07 14:31:03 +0300

    Channel

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,57 +3,52 @@ package repository
 import (
 	"fmt"
 	"homework7/internal/model"
+	"sync"
 )
 
-// var adminka []model.Adminka
-var order []*model.Order
-var product []*model.Product
-var category []*model.Category
-var user []*model.User
-var adminka []*model.Adminka
+var (
+	order    []*model.Order
+	product  []*model.Product
+	category []*model.Category
+	user     []*model.User
+	adminka  []*model.Adminka
+	mu       sync.Mutex
+)
 
-// func AddData(data model.Orders) {
-func AddData(data model.ID) {
-	switch v := data.(type) {
-	case model.Order:
-		order = append(order, &v)
-		fmt.Println("add Order", v)
-	case model.Product:
-		product = append(product, &v)
-		fmt.Println("add Product", v)
-	case model.Category:
-		category = append(category, &v)
-		fmt.Println("add Category", v)
-	case model.User:
-		user = append(user, &v)
-		fmt.Println("add User", v)
-	case model.Adminka:
-		adminka = append(adminka, &v)
-		fmt.Println("add Adminka", v)
-	default:
-		fmt.Printf("Неопределенный тип данных: %T/n", v)
+// ReceiveData читает из канала и добавляет в соответствующий слайс.
+func ReceiveData(ch <-chan model.ID, done chan<- struct{}) {
+	defer close(done)
+	for v := range ch {
+		switch elm := v.(type) {
+		case model.Order:
+			order = append(order, &elm)
+			fmt.Println("Добавлен Order: ", elm)
+		case model.Product:
+			product = append(product, &elm)
+			fmt.Println("Добавлен Product: ", elm)
+		case model.Category:
+			category = append(category, &elm)
+			fmt.Println("Добавлен Category: ", elm)
+		case model.User:
+			user = append(user, &elm)
+			fmt.Println("Добавлен User: ", elm)
+		case model.Adminka:
+			adminka = append(adminka, &elm)
+			fmt.Println("Добавлен Adminka: ", elm)
+		default:
+			fmt.Println("Неизвестный тип: ", elm)
+		}
 	}
-
 }
 
-//var OrderSlice []model.Order // слайс структур model.Order
-
-// Описание подходящего нам интерфейса
-//type ID interface {
-//	GetID() int64
-//}
-
-// функция принимает любые структуры, если они имплементируют (предоставляют) метод GetID() int64
-//func Store(input ID) {
-//	switch typed := input.(type) { // с помощью .(type) получаем реальный тип input, по сути снимаем мантию интерфейса
-
-//	case model.Order:
-//
-// append в нужный слайс
-//
-//	OrderSlice = append(OrderSlice, typed)
-//
-// и так далее
-//
-//	}
-//}
+// Snapshot возвращает слайсы для безопасного чтения.
+func Snapshot() ([]*model.Order, []*model.Product, []*model.Category, []*model.User, []*model.Adminka) {
+	mu.Lock()
+	defer mu.Unlock()
+	os := append([]*model.Order(nil), order...)
+	ps := append([]*model.Product(nil), product...)
+	cs := append([]*model.Category(nil), category...)
+	us := append([]*model.User(nil), user...)
+	as := append([]*model.Adminka(nil), adminka...)
+	return os, ps, cs, us, as
+}
